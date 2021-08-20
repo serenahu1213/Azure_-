f516c65a65d587780016664b2bc2a92b5468d3f2
serenahu1213 <48993008+serenahu1213@users.noreply.github.com>
2021-08-20 12:13:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # imgur key
-client_id = 'f4c26dfe3745eda'
-client_secret = '21ef5f5a3830e2e5f4049b2b8bb81080dc6b147e'
-album_id = 'J5ROOcW'
-access_token = '7809e56e26fdf4d5f4f5aa2fa602416b672c66fc'
-refresh_token = 'ec73e31f2dd597cdbf052613d244edc0325a7fd8'
+client_id = ''
+client_secret = ''
+album_id = ''
+access_token = ''
+refresh_token = ''
 
 # line bot key
-line_channel_access_token = 'XBKs4QPm4vfsX0qxZd2pyhZMEYVBs1xbv8e+Z4mPRDzK2OqJ4UWVY3g3nUiDgP5iebaaN7xFb8Ks561E8EzAt4hbAozzbHsRMOJXS6eLeUYsEN28bASr13bru49B23xUzBMWeuh1N7vLUBZuCYFXZQdB04t89/1O/w1cDnyilFU='
-line_channel_secret = '6538f1908ff5877945040220adaa8253'
+line_channel_access_token = ''
+line_channel_secret = ''
